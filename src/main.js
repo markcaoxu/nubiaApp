f482d8d8c23e0281c5388e9ff0e6cadc9c6a696b
wangyan <395842085@qq.com>
2019-10-17 00:47:59 +0800

    第一次提交，“发现”页面的静态css以及点击导航栏，路由切换

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import Vue from 'vue'
 import App from './App'
 // 引入路由
 import router from './router/index.js'
+//引入reset.css文件，重置样式
+import '../static/css/reset.css'
+//引入字体图标文件
+import '../static/css/font-icon.css'
+
 
 // 设置vue配置信息
 Vue.config.productionTip = false
--- src/router/routes.js
+++ src/router/routes.js
@@ -3,7 +3,16 @@ import Classify from '../pages/Classify/Classify.vue'
 import Find from '../pages/Find/Find.vue'
 import Shopcar from '../pages/Shopcar/Shopcar.vue'
 import Profile from '../pages/Profile/Profile.vue'
-
+//引入详情页组件
+import Detail from '../pages/Find/Detail.vue'
+//引入体验店组件
+import ExperienceShop from '../pages/Find/ExperienceShop.vue'
+//引入视频长廊组件
+import Video from '../components/Find/Video.vue'
+//一如产品评价组件
+import ProductEvaluation from '../components/Find/ProductEvaluation'
+//引入新闻中心组件
+import NewsCenter from '../components/Find/NewsCenter.vue'
 
 
 export default [
@@ -27,8 +36,30 @@ export default [
   { 
     // 发现 find
     path: '/find',
-    component: Find
-  },
+		component: Find,
+		children:[
+			{
+				path:'/find/detail',
+				component: Detail
+			},
+			{
+				path:'/find/experienceShop',
+				component: ExperienceShop
+			},
+			{
+				path:'/find/video',
+				component: Video
+			},
+			{
+				path:'/find/productEvaluation',
+				component:ProductEvaluation
+			},
+			{
+				path:'/find/newsCenter',
+				component:NewsCenter
+			}
+		]
+	},
   {
     // 购物车 shopcar
     path: '/shopcar',
