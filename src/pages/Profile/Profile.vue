230a5ce3b644cfb058bde14b9da0541c236042ae
sword <markcaoxu@163.com>
2019-10-16 12:55:27 +0800

    profile分支

--- src/pages/Profile/Profile.vue
+++ src/pages/Profile/Profile.vue
@@ -1,6 +1,29 @@
 <template>
-  <div>
-    个人中心
+  <!-- profile 个人中心组件 -->
+  <div class="user_center">
+
+    <!-- 头部 -->
+    <div class="user_header">
+      <!-- 头像 -->
+      <img class="user_img" src="" alt="">
+      <!-- 用户名-会员等级 -->
+      <div class="user_name">
+        <p class="user_text">骑士</p>
+        <div class="user_vip">
+          <img class="user_star" src="" alt="">
+          <p>xxxx</p>
+        </div>
+      </div>
+
+    </div>
+    <!-- 券 -->
+    <div class="user_ticket"></div>
+    <!-- 订单 -->
+    <div class="user_order"></div>
+    <!-- 我的工具 -->
+    <div class="user_tool"></div>
+    <!-- 售后服务 -->
+    <div class="user_serve"></div>
   </div>
 </template>
 
@@ -10,6 +33,10 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="stylus" rel="stylesheet/stylus">
+.user_header
+  width 100%
+  height 120px
+  background-color #02a774
+ 
 </style>
