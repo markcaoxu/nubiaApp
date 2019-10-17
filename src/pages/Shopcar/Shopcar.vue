b04baf2d470d85bc118da23924b8e2fe820faa2a
guanzhen <406873093@qq.com>
2019-10-17 16:47:31 +0800

    购物车updata

--- src/pages/Shopcar/Shopcar.vue
+++ src/pages/Shopcar/Shopcar.vue
@@ -1,8 +1,8 @@
 <template>
 <div class="shopcar">
   <!-- 购物车中有物品 加载结算 没有加载另一页面 -->
-  <EmtyShopcar v-if="isEmty" :hotList="hotList"/>
-  <HaveShopcar v-if="!isEmty" :checked_pone="checked_pone" :have_rec="have_rec"/>
+  <EmtyShopcar v-show="isEmty" :hotList="hotList" />
+  <HaveShopcar v-show="!isEmty" :checked_pone="checked_pone" :have_rec="have_rec" />
 </div>  
 </template>
 
@@ -21,43 +21,47 @@ export default {
   },
   data(){
     return{
-      // checked_pone:[], // 选购的商品
       hotList:[], // 空购物车中的 热销推荐   需要vuex
       have_rec:[], // 非空购物车 组件中的推荐商品
       isEmty:true  //购物车是否为空
     }
   },
   computed:{
-    
-    ...mapState(['checked_pone'])
+    ...mapState({
+      checked_pone:function(state){
+       return state.checked_pone
+        }
+    })
+  },
+  methods:{
+    // 添加商品
+    addPoneByShopcar(pone){
+      this.$store.dispatch('addCheckedShop',pone)
+    },
+    // 判断购物车是否为空
+    shopcarIsEmty(){
+      if(this.checked_pone[0]!==undefined&&this.checked_pone.length>0){
+        this.isEmty=false
+        // 如果有货 tabbar 不显示
+          this.$route.meta.isShowFooter=false
+      }else{
+        this.$route.meta.isShowFooter=true
+        this.isEmty=true
+      }
+    }
   },
   mounted(){
-
-    // console.log(this.$route)
-    // 获取选中的商品
-    // this.checked_pone=shopcar_data.checked_pone
     // 获取热销商品
     this.hotList=shopcar_data.hotSale
     // 获取推荐商品
     this.have_rec=shopcar_data.have_rec
-    // this.$route.meta.isShowFooter=false
+    
   },
   watch:{
   
       //判断购物车是否为空
     checked_pone:function(){
-      if(this.checked_pone[0]!==undefined&&this.checked_pone.length>0){
-        this.isEmty=false
-        // 如果有货 tabbar 不显示
-          this.$route.meta.isShowFooter=false
-        
-      }else{
-        this.$route.meta.isShowFooter=true
-        this.isEmty=true
-      
-      }
-     
-      
+      this.shopcarIsEmty()
     }
   }
 }
--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,5 @@
 // 存储 state 状态数据
 export default{
-  checked_pone:[] // 选购的商品 
+  checked_pone:[], // 选购的商品 
   
 }
