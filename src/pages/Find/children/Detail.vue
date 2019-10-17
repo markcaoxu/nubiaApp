45fbf9fe1cced68c6ace85f84b663e97cbd64407
wangyan <395842085@qq.com>
2019-10-17 18:47:19 +0800

    详情页样式搭建

--- src/pages/Find/children/Detail.vue
+++ src/pages/Find/children/Detail.vue
@@ -1,13 +1,22 @@
 <template>
 <div>
-	详情页
+	<section>
+		<h1></h1>
+		<h6></h6>
+		<input type="select">
+		<input type="select">
+		<img src="" alt="">
+		<BlockShop />
+	</section>
 	
 </div>
 </template>
 <script>
-
+import BlockShop from '../../../components/Find/BlockShop'
 export default {
-
+	computed:{
+		BlockShop
+	}
 }
 </script>
 <style lang='stylus'>
