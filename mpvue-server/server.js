f2dd4c671b8dc88e14b31babbae9023a31cc8af4
guanzhen <406873093@qq.com>
2019-10-17 20:41:55 +0800

    购物车基本完成数据updata

--- mpvue-server/server.js
+++ mpvue-server/server.js
@@ -9,7 +9,6 @@ let koaRouter = new KoaRouter()
 
 // 引入datas数据
 let datas = require('./datas/login.json')
-
 // 引入shopcar数据 热销数据跟 推荐数据
 let shopHotData =require('./datas/shopcar-data.json')
 
@@ -22,21 +21,36 @@ koaRouter.post('/login_pwd',(ctx)=>{
         message:'登录成功',
         code:"0"
       }
-    
+    }
+  })
+})
+// 购物车 热品推荐信息
+koaRouter.get('/shopcar',((ctx)=>{
+  let { hotSale,have_rec } = shopHotData
+  console.log(ctx.query)
+  if(ctx.query.q=='hot'&&ctx.query.b=='have'){
+    ctx.body={
+      message:{hotSale,have_rec},
+      code:"0"
+    }
+  }
+   
+})
+)
+// 首页 信息
+koaRouter.get('/msite',((ctx)=>{
+  let {msite} = shopHotData
+  let {msite_swiper,hotPone} =msite
+  console.log(ctx.query)
+  if(ctx.query.q=='lbt'&&ctx.query.b=='hot'){
+    ctx.body={
+      // 轮播图和 热销机型 /msite?q=lbt&b=hot'
+      message:{msite_swiper,hotPone},
+      code:"0"
+    }
   }
 })
-// 响应shop hot 数据
-koaRouter.get('/shopcar',(ctx)=>{
-  console.log(ctx.query);
-  
-    let {hotSale}=shopHotData
-      ctx.body={
-        message:hotSale,
-        code:"0"
-      }
-  
-})
-
+)
 // 声明使用所有的路由及路由的相关的所有的方法
 koa
   .use(koaRouter.routes())
