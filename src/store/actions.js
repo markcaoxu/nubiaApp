180b8604c5ae07a382845366fff1076822fbb5ad
sword <markcaoxu@163.com>
2019-10-17 18:32:56 +0800

    vuex保存用户数据起步

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,6 +1,6 @@
 // 存储 actions 间接操作状态数据的集合
 // 引入常量名
-import {ADD_CHECKED_PONE,DEL_CHECKED_PONE,REMOVE_CHECKED_PONE} from './mutationsType'
+import {ADD_CHECKED_PONE,DEL_CHECKED_PONE,REMOVE_CHECKED_PONE, UPDATE_USER, REMOVE_USER} from './mutationsType'
 export default{
   // 购物车添加商品
   addCheckedShop({commit},pone){
@@ -13,5 +13,16 @@ export default{
   // 删
   removeCheckedShop({commit},pone){
     commit(REMOVE_CHECKED_PONE,{pone})
+  },
+
+  // 更新保存用户信息
+  saveUser({ commit },user){
+    // 将输入的user更新到vuex
+    commit(UPDATE_USER,user)
+  },
+  // 清除用户信息
+  removeUser({ commit }){
+    // 重置用户信息
+    commit(REMOVE_USER)
   }
 }
