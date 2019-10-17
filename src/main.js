e8a5abcd2daee66ae94b7215869eda67fe3c959a
wangyan <395842085@qq.com>
2019-10-17 08:54:58 +0800

    第一次提交，“发现”页面的静态css以及点击导航栏，路由切换

--- src/main.js
+++ src/main.js
@@ -5,9 +5,7 @@ import App from './App'
 // 引入路由
 import router from './router/index.js'
 //引入reset.css文件，重置样式
-import '../static/css/reset.css'
-//引入字体图标文件
-import '../static/css/font-icon.css'
+import '../public/css/reset.css'
 
 
 // 设置vue配置信息
