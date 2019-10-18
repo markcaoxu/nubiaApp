30cafe266b11add0bb96a95a270e0c5f42367179
wangyan <395842085@qq.com>
2019-10-18 17:53:51 +0800

    修改样式

---
+++ src/components/Find/BlockSuggest.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id='app'>
+    <section class="footerr">
+      <p class="suggest">相关推荐</p>
+      <ul class="list">
+        <li class="show">
+          <img class="img" src="" alt="">
+          <h3 class="h3">『玩转nubia UI』个性熄屏，自己掌控！</h3>
+          <div class="dtv">
+            <span>努比亚</span>
+            <span>2019-05-04</span>
+          </div>
+        </li>
+      </ul>
+      <p class="btn">查看更多文章 》</p>
+    </section>
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
+<style lang='stylus'>
+.show
+  padding 15px
+  height 100px
+  .img
+    width 100%
+  .h3
+    display inline-block
+    line-height 30px
+    font-size 25px
+  .dtv
+    height 50px
+</style>
--- src/pages/Find/children/Detail.vue
+++ src/pages/Find/children/Detail.vue
@@ -5,8 +5,8 @@
         <span class='lt' @click="goTo('/find/productEvaluation')">&lt;</span>
         <span class='txt'>产品热评</span>
       </header>
-      <h3 class="title">『玩转nubia UI』个性熄屏，自己掌控！</h3>
-      <div class='notes'>
+      <h3 class="titlee">『玩转nubia UI』个性熄屏，自己掌控！</h3>
+      <div class='notess'>
         <span>努比亚</span>
         <span>2019-05-04</span>
       </div>
@@ -14,31 +14,17 @@
         <p>努比亚z20作为今年努比亚品牌的旗舰机型，外观上继承了去年努比亚x上独特的双面屏设计，这次的Z20又给我们带来怎么样的改变呢。那么跟随行行一起往下看！</p>
         <img src="https://static-bbs.nubia.cn/forum/201908/16/160504pmhhhtptfptifnv9.png.thumb.jpg" alt="">
       </div>
-			<div class="content">
-        <p>努比亚z20作为今年努比亚品牌的旗舰机型，外观上继承了去年努比亚x上独特的双面屏设计，这次的Z20又给我们带来怎么样的改变呢。那么跟随行行一起往下看！</p>
-        <img src="https://static-bbs.nubia.cn/forum/201908/16/160504pmhhhtptfptifnv9.png.thumb.jpg" alt="">
-      </div>
-			<div class="content">
-        <p>努比亚z20作为今年努比亚品牌的旗舰机型，外观上继承了去年努比亚x上独特的双面屏设计，这次的Z20又给我们带来怎么样的改变呢。那么跟随行行一起往下看！</p>
-        <img src="https://static-bbs.nubia.cn/forum/201908/16/160504pmhhhtptfptifnv9.png.thumb.jpg" alt="">
-      </div>
-			<div class="content">
-        <p>努比亚z20作为今年努比亚品牌的旗舰机型，外观上继承了去年努比亚x上独特的双面屏设计，这次的Z20又给我们带来怎么样的改变呢。那么跟随行行一起往下看！</p>
-        <img src="https://static-bbs.nubia.cn/forum/201908/16/160504pmhhhtptfptifnv9.png.thumb.jpg" alt="">
-      </div>
-			<div class="content">
-        <p>努比亚z20作为今年努比亚品牌的旗舰机型，外观上继承了去年努比亚x上独特的双面屏设计，这次的Z20又给我们带来怎么样的改变呢。那么跟随行行一起往下看！</p>
-        <img src="https://static-bbs.nubia.cn/forum/201908/16/160504pmhhhtptfptifnv9.png.thumb.jpg" alt="">
-      </div>
-			<div class="content">
-        <p>努比亚z20作为今年努比亚品牌的旗舰机型，外观上继承了去年努比亚x上独特的双面屏设计，这次的Z20又给我们带来怎么样的改变呢。那么跟随行行一起往下看！</p>
-        <img src="https://static-bbs.nubia.cn/forum/201908/16/160504pmhhhtptfptifnv9.png.thumb.jpg" alt="">
-      </div>
+      <BlockSuggest />
+      <router-view></router-view>
     </section>
   </div>
 </template>
 <script>
+import BlockSuggest from '../../../components/Find/BlockSuggest'
 export default {
+  computed: {
+    BlockSuggest
+  }
 }
 </script>
 <style lang='stylus'>
@@ -63,18 +49,21 @@ export default {
     font-family 'nubia'
     margin 0 6px
     color #464646
-.title
-	font-size 18px
+.titlee
+  font-size 25px
   line-height 30px
-  font-weight 500
-.notes
+  font-weight 700
+	margin 10px auto
+.notess
   color #777
   font-size 14px
   margin-top 10px
 .content
-	width 100%
-	p
-		font-size 20px
-	img 
-		width 100%
+  background-color white
+  p
+    font-size 20px
+		box-sizing border-box
+
+  img
+    width 100%
 </style>
